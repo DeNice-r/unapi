6b93486de798f01392aee8da06e015216dfbeab3
Denys Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2023-04-28 14:19:13 +0300

    Added image support to facebook

--- unapi/platforms/facebook/model.py
+++ unapi/platforms/facebook/model.py
@@ -1,5 +1,10 @@
+import logging
 from typing import List
-from pydantic import BaseModel
+
+import requests
+from pydantic import BaseModel, validator
+
+from unapi.util import save_image
 
 
 class Sender(BaseModel):
@@ -10,9 +15,39 @@ class Recipient(BaseModel):
     id: str
 
 
+class PayloadItem(BaseModel):
+    url: str
+    local_path: str | None = None
+
+
+class AttachmentItem(BaseModel):
+    type: str
+    payload: PayloadItem
+
+    @validator('type')
+    def validate_type(cls, v):
+        if v not in ['image']:  # audio, video, file, location, fallback
+            message = f'Invalid attachment type: {v}'
+            logging.error(message)
+            raise ValueError(message)
+        return v
+
+    @validator('payload')
+    def validate_payload(cls, v):
+        if not v:
+            raise ValueError('Payload is empty')
+
+        response = requests.get(v.url)
+        if response.ok:
+            file_path = response.url.split('/')[-1].split('?')[0]
+            v.local_path = save_image(file_path, response.content)
+        return v
+
+
 class Message(BaseModel):
     mid: str
-    text: str
+    text: str = ''
+    attachments: List[AttachmentItem] | None
 
 
 class MessagingItem(BaseModel):
