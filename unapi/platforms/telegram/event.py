2c87e1acf40cd9a373299e64166d0c8b41e76279
Denys Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2023-06-21 21:40:30 +0300

    Removed comment

--- unapi/platforms/telegram/event.py
+++ unapi/platforms/telegram/event.py
@@ -43,36 +43,6 @@ class TelegramEvent(Event):
         )
         return attachments
 
-    # Telegram attachment downloading example:
-
-    # @validator('photo')
-    # def download_attachment(cls, value):
-    #     if value is None:
-    #         return value
-    #
-    #     photo = value[-1]  # Only last variant is used as it is the biggest one
-    #     if photo.local_path:
-    #         return value
-    #     file_id = photo.file_id
-    #     file_url = f"https://api.telegram.org/bot{telegram_token}/getFile?file_id={file_id}"
-    #     response = requests.get(file_url)
-    #     response_json = response.json()
-    #
-    #     if response_json["ok"]:
-    #         file_path = response_json["result"]["file_path"]
-    #         file_url = f"https://api.telegram.org/file/bot{telegram_token}/{file_path}"
-    #         response = requests.get(file_url)
-    #
-    #         if response.ok:
-    #             photo.local_path = save_image(file_path, response.content)
-    #         else:
-    #             raise ValueError(f"Error downloading file: {response.status_code} {response.reason}")
-    #     else:
-    #         raise ValueError(
-    #             f"Error getting file path: {response_json['error_code']} {response_json['description']}")
-    #
-    #     return value
-
     @staticmethod
     async def is_request_authentic(request: Request) -> bool:
         try:
